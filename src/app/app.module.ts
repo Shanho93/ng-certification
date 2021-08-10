ba17934f7fcc3d7e7928b1818c5ac17d7b7e4581
Alexander Bilinski <alexander.bilinski.93@gmail.com>
2021-08-10 15:46:26 +0200

    Initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { ForecastComponent } from './views/forecast/forecast.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, WeatherCardComponent, ForecastComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
